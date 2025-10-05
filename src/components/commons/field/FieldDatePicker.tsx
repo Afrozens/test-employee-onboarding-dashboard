189f98d45f4a1de1ccf0857fec3a69e54371fa4b
Jesus Chacon <jesuszen2.6@gmail.com>
2025-10-05 11:57:57 -0400

    feat: add logic for field date picker

---
+++ src/components/commons/field/FieldDatePicker.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { DatePicker } from 'antd';
+import { ControllerRenderProps } from 'react-hook-form';
+import dayjs from 'dayjs';
+import { RangePickerProps } from 'antd/es/date-picker';
+
+import FieldError from './FieldError';
+import { convertDateFormat, reformedQueryDate } from '@/utils/datetime';
+
+interface Props {
+  label: string;
+  id: string;
+  disabled?: boolean;
+  field?: ControllerRenderProps<any, string>;
+  classAditional?: string;
+  isRequired?: boolean;
+  disabledDate?: any;
+  value?: string;
+  error?: string;
+}
+
+const FieldDatePicker = ({
+  classAditional,
+  error,
+  value,
+  disabled = false,
+  disabledDate: disabledDateOutside,
+  field,
+}: Props) => {
+  const formatDate = (date: string) => {
+    const parsed = dayjs(reformedQueryDate(date), 'YYYY-MM-DD');
+    return parsed;
+  };
+
+  const handleSingleChange = (date: any) => {
+    if (date) {
+      const formattedDate = convertDateFormat(new Date(date));
+      field?.onChange(formattedDate);
+    } else {
+      field?.onChange('');
+    }
+  };
+
+  const disabledDate: RangePickerProps['disabledDate'] = (current) => {
+    return current && current < dayjs().endOf('day');
+  };
+
+  const getPickerValue = () => {
+      if (value && typeof value === 'string') {
+        return formatDate(value);
+      }
+      return undefined;
+  };
+
+  return (
+    <div
+      className={`${
+        classAditional || ''
+      } flex w-full flex-col items-center justify-center`}
+    >
+          <DatePicker
+            needConfirm={false}
+            disabledDate={disabledDateOutside ?? disabledDate}
+            allowClear={false}
+            disabled={disabled}
+            inputReadOnly
+            value={getPickerValue() as dayjs.Dayjs | undefined}
+            showTime={false}
+            onChange={handleSingleChange}
+          />
+      <FieldError error={error} />
+    </div>
+  );
+};
+
+export default FieldDatePicker;
